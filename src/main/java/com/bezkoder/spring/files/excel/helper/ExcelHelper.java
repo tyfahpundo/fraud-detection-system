002a81bf69b76d30512176a3ee2b4f532753a3f0
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-04-22 17:37:22 +0200

    Implemented Flag options corresponding to percentages

--- src/main/java/com/bezkoder/spring/files/excel/helper/ExcelHelper.java
+++ src/main/java/com/bezkoder/spring/files/excel/helper/ExcelHelper.java
@@ -4,12 +4,19 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFExtendedColor;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -31,13 +38,12 @@ public class ExcelHelper {
 
     try (Workbook workbook = new XSSFWorkbook();
          ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-
-
       Sheet sheet = workbook.createSheet(SHEET);
-
+      CellStyle style = workbook.createCellStyle();
+      style.setFillBackgroundColor(IndexedColors.RED.getIndex());
+      style.setFillPattern(FillPatternType.BIG_SPOTS);
       // Header
       Row headerRow = sheet.createRow(0);
-
       for (int col = 0; col < HEADERs.length; col++) {
         Cell cell = headerRow.createCell(col);
         cell.setCellValue(HEADERs[col]);
@@ -75,6 +81,61 @@ public class ExcelHelper {
         row.createCell(4).setCellValue(count);
         double percentage =((double) count/digitList.size())*100;
         row.createCell(5).setCellValue(String.valueOf(percentage));
+
+        switch((int) row.getCell(4).getNumericCellValue()){
+          case 1:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(30.1)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+
+          case 2:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(17.6)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+          case 3:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(12.5)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+          case 4:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(9.7)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+
+          case 5:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(7.9)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+
+          case 6:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(6.7)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+          case 7:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(5.8)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+          case 8:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(5.1)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+
+          case 9:
+            if(!row.getCell(5).getStringCellValue().equals(String.valueOf(4.6)) ){
+              row.getCell(5).setCellStyle(style);
+            }
+            break;
+
+          default:
+            break;
+        }
         row.createCell(6).setCellValue(String.valueOf(Math.log10(((double) 1/firstDigit)+1)));
         row.createCell(7).setCellValue(String.valueOf(percentage-(Math.log10(((double) 1/firstDigit)+1))));
 
